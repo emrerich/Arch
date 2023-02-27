5f26a0487565da90629e5330c295805e1da5a422
emrerich <emre.zengin5@ogr.sakarya.edu.tr>
2023-02-27 16:50:46 +0300

    Ana Sayfa İyileştirildi
    27 feb

--- JS/index.js
+++ JS/index.js
@@ -26,3 +26,53 @@ const navbarLinks = document.getElementsByClassName('navbar-links')[0]
 toggleButton.addEventListener('click', ()=> {
   navbarLinks.classList.toggle('active')
 })
+
+
+// slider
+var slides= document.querySelectorAll('.slide');
+var btns= document.querySelectorAll('.btn');
+let currentSlide=1;
+
+var manualNav = function(manual){
+    slides.forEach((slide)=>{
+        slide.classList.remove('active');
+
+        btns.forEach((btn)=>{
+            btn.classList.remove('active');
+
+        });
+    });
+    slides[manual].classList.add('active');
+    btns[manual].classList.add('active');
+}
+btns.forEach((btn,i)=>{
+    btn.addEventListener("click",()=>{
+        manualNav(i);
+        currentSlide=i;
+    });
+});
+
+
+var repeat = function(activeClass){
+    let active = document.getElementsByClassName('active');
+    let i = 1;
+    var repeater = () => {
+        setTimeout(function(){
+            [...active].forEach((currentSlide)=>{
+                currentSlide.classList.remove('active');
+            });
+            slides[i].classList.add('active');
+            btns[i].classList.add('active');
+            i++;
+            if(slides.length==i){
+                i=0;
+            }
+            if(i>=slides.length){
+                return;
+            }
+            repeater();
+        },13000);
+    }
+    repeater();
+}
+repeat();
